940c1034767bacdab51911deea9b8fa5cdb541fb
CarlNxumalo <carl.ybongani@gmail.com>
2024-12-23 21:17:40 +0200

    pdfguy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+		  external: ['pdf-lib']
+		}
+	  },
 	ssr: {
 		noExternal: ['flowbite-svelte'],
 	  },
